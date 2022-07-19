be203620635f22aa7c50d405ada202e745a82bbc
Tri Nguyen <mr.tringuyen1225@gmail.com>
2022-07-19 11:25:15 -0700

    register user route

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,9 +5,24 @@ const router = express.Router()
 // route     POST api/users
 // desc      Register user
 // access    Public
-router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('User route')
-})
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is required').isEmail(),
+    check('password', 'Enter a password with 6 or more characters').isLength({
+      min: 6,
+    }),
+  ],
+
+  (req, res) => {
+    // if those above don't match then res error message
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    res.send('User route')
+  }
+)
 
 module.exports = router
